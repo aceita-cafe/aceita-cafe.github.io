e9f55979873b381de16cda6df7f259a68fa104e2
PinhoOn <pinhoonjob@gmail.com>
2021-04-30 18:00:15 -0300

    modal styles

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,25 @@
 import { data } from "../data/modal.json";
 
-const variationElement = document.querySelector("[data-variation]");
+const coffeeName = document.querySelector("[data-name]");
+const coffeeVariation = document.querySelector("[data-variation]");
+const coffeeSensory = document.querySelector("[data-sensory");
+const coffeeProcess = document.querySelector("[data-process");
+const coffeeHighLights = document.querySelector("[data-highlights");
+const coffeeMadeBy = document.querySelector("[data-madeby");
+const coffeeAltitude = document.querySelector("[data-altitude");
+const coffeeRegion = document.querySelector("[data-region");
+
 
 const openModal = (id) => {
-  variationElement.innerHTML = data[id][1];
+  coffeeName.innerHTML = data[id].name;
+  coffeeVariation.innerHTML = data[id].variation;
+  coffeeSensory.innerHTML = data[id].sensory;
+  coffeeProcess.innerHTML = data[id].process;
+  coffeeHighLights.innerHTML = data[id].highlights;
+  coffeeMadeBy.innerHTML = data[id].madeby;
+  coffeeAltitude.innerHTML = data[id].altitude;
+  coffeeRegion.innerHTML = data[id].region;
 };
 
 window.openModal = openModal;
+

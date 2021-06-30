0592c9203045998501c419651836eb0d8e76b80d
PinhoOn <pinhoonjob@gmail.com>
2021-06-30 15:56:00 -0300

    Buy button hotfix

--- src/js/showcase.js
+++ src/js/showcase.js
@@ -9,7 +9,7 @@ export const showcaseList = (showcase) => {
     <div class="showcase__container">
       <h5 class="showcase__name">${showcase.name}</h5>
       <button class="button button--stroke button--details" onclick="openModal(${showcase.id})">Detalhes</button>
-      <a class="button button--whatsapp" href="https://wa.me/556699511499" target="_blank" rel="noopener">Comprar</a>
+      <a class="button button--whatsapp" href="https://wa.me/556699511499" target="_blank">Comprar</a>
     </div>
   </li>
   `
